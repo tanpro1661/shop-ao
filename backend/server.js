933ee2a317d38e42acb7bb027a04d452a1ecf98c
tanpro1998 <davidtanpro1998@gmail.com>
2022-03-29 10:12:48 +0700

    commit

--- backend/server.js
+++ backend/server.js
@@ -23,18 +23,22 @@ app.use("/api/carts", cartRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/checkout", stripeRouter);
 
-const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// const __dirname = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running");
+//   });
+// }
+
+app.get("/", (req, res) => {
+  res.send("APP is running")
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
