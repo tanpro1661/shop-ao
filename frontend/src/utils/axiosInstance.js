207558182790225b9b3054b5dda0e59570bc3484
tanpro1998 <davidtanpro1998@gmail.com>
2022-04-11 18:35:04 +0700

    commit

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import jsCookie from "js-cookie";
+import jwt_decode from "jwt-decode";
 
 const BASE_URL = "http://localhost:5000/api";
 
-const TOKEN = jsCookie.get("access");
+const ACCESS_TOKEN = jsCookie.get("access");
 const REFRESH_TOKEN = jsCookie.get("refresh");
 
 export const publicRequest = axios.create({
@@ -12,5 +13,31 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { authorization: `Bearer ${TOKEN}` },
+  headers: { authorization: `Bearer ${ACCESS_TOKEN}` },
 });
+
+const refreshToken = async () => {
+  try {
+    const res = await axios.post(`${BASE_URL}/users/refresh`, {
+      token: REFRESH_TOKEN,
+    });
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+userRequest.interceptors.request.use(
+  async (config) => {
+    let currentDate = new Date();
+    const decodedToken = jwt_decode(ACCESS_TOKEN);
+    if (decodedToken.exp * 1000 < currentDate.getTime()) {
+      const data = await refreshToken();
+      config.headers.authorization = "Bearer " + data.accessToken;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
